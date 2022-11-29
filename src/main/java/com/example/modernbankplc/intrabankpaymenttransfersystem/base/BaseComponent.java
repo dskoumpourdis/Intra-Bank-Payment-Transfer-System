f8b929f33a2eab49f495ff10545d731f7c548b30
Dimitris <nomix_xx@hotmai.com>
2022-11-29 19:54:55 +0000

    Add abstract class for logging.

---
+++ src/main/java/com/example/modernbankplc/intrabankpaymenttransfersystem/base/BaseComponent.java
@@ -0,0 +1,8 @@
+package com.example.modernbankplc.intrabankpaymenttransfersystem.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseComponent {
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+}
