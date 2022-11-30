abc2e9236b4e29ad3f2f15196ef52fa12b6ad164
Dimitris <nomix_xx@hotmai.com>
2022-11-30 18:03:10 +0000

    Add /id/statements and /id/balance endpoints to account controller.

--- src/main/java/com/example/modernbankplc/intrabankpaymenttransfersystem/controller/AccountController.java
+++ src/main/java/com/example/modernbankplc/intrabankpaymenttransfersystem/controller/AccountController.java
@@ -5,8 +5,14 @@ import com.example.modernbankplc.intrabankpaymenttransfersystem.domain.Account;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.mapper.AccountMapper;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.service.AccountService;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.service.BaseService;
+import com.example.modernbankplc.intrabankpaymenttransfersystem.transfer.ApiResponse;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.transfer.resource.AccountResource;
+import com.example.modernbankplc.intrabankpaymenttransfersystem.transfer.resource.BalanceResource;
+import com.example.modernbankplc.intrabankpaymenttransfersystem.transfer.resource.StatementResource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +32,17 @@ public class AccountController extends BaseController<Account, AccountResource> 
 	@Override
 	protected BaseMapper<Account, AccountResource> getMapper() {
 		return accountMapper;
+	}
+
+	@GetMapping("/{id}/balance")
+	public ResponseEntity<ApiResponse<BalanceResource>> getBalance(@PathVariable("id") final Long id) {
+		return ResponseEntity.ok(
+				ApiResponse.<BalanceResource>builder().data(getMapper().toResource(getBaseService().get(id)).getBalance()).build());
+	}
+
+	@GetMapping("/{id}/statements")
+	public ResponseEntity<ApiResponse<StatementResource>> getStatement(@PathVariable("id") final Long id) {
+		return ResponseEntity.ok(
+				ApiResponse.<StatementResource>builder().data(getMapper().toResource(getBaseService().get(id)).getStatement()).build());
 	}
 }
