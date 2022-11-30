80ed03898bb52fe1aee41f0ed21b7d7c2c398823
Dimitris <nomix_xx@hotmai.com>
2022-11-30 15:50:23 +0000

    Add ApiError and ApiResponse classes.

---
+++ src/main/java/com/example/modernbankplc/intrabankpaymenttransfersystem/transfer/ApiError.java
@@ -0,0 +1,12 @@
+package com.example.modernbankplc.intrabankpaymenttransfersystem.transfer;
+
+import lombok.ToString;
+import lombok.Value;
+
+@Value
+@ToString
+public class ApiError {
+    Integer status;
+    String message;
+    String path;
+}
