3a2aeda572ecad11890278d130ef81471eb1eb2f
Dimitris <nomix_xx@hotmai.com>
2022-11-30 18:26:50 +0000

    Extract values to constants, refactor accountId to accountNum

--- src/main/java/com/example/modernbankplc/intrabankpaymenttransfersystem/dataload/JpaRunner.java
+++ src/main/java/com/example/modernbankplc/intrabankpaymenttransfersystem/dataload/JpaRunner.java
@@ -6,7 +6,6 @@ import com.example.modernbankplc.intrabankpaymenttransfersystem.domain.Balance;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.domain.Statement;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.domain.Transaction;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.domain.TransactionType;
-import com.example.modernbankplc.intrabankpaymenttransfersystem.repository.AccountRepository;
 import com.example.modernbankplc.intrabankpaymenttransfersystem.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -14,13 +13,16 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.List;
 import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class JpaRunner extends BaseComponent implements CommandLineRunner {
 
+	public static final int BALANCE_AMOUNT = 1000;
+	public static final long ACCOUNT_ID_2 = 222L;
+	public static final long ACCOUNT_ID_1 = 111L;
+	public static final int TRANSACTION_AMOUNT = 100;
 	private final AccountService accountService;
 
 	@Override
@@ -28,30 +30,30 @@ public class JpaRunner extends BaseComponent implements CommandLineRunner {
 		Set<Transaction> transactionList_1 = Set.of(
 			Transaction.builder()
 				.transactionType(TransactionType.DEBIT)
-				.amount(BigDecimal.valueOf(100))
+				.amount(BigDecimal.valueOf(TRANSACTION_AMOUNT))
 				.currency(Currency.getInstance("GBP"))
-				.accountId(222L)
+				.accountNum(ACCOUNT_ID_2)
 				.build(),
 			Transaction.builder()
 				.transactionType(TransactionType.CREDIT)
-				.amount(BigDecimal.valueOf(100))
+				.amount(BigDecimal.valueOf(TRANSACTION_AMOUNT))
 				.currency(Currency.getInstance("GBP"))
-				.accountId(222L)
+				.accountNum(ACCOUNT_ID_2)
 				.build()
 		);
 
 		Set<Transaction> transactionList_2 = Set.of(
 			Transaction.builder()
 				.transactionType(TransactionType.DEBIT)
-				.amount(BigDecimal.valueOf(100))
+				.amount(BigDecimal.valueOf(TRANSACTION_AMOUNT))
 				.currency(Currency.getInstance("GBP"))
-				.accountId(111L)
+				.accountNum(ACCOUNT_ID_1)
 				.build(),
 			Transaction.builder()
 				.transactionType(TransactionType.CREDIT)
-				.amount(BigDecimal.valueOf(100))
+				.amount(BigDecimal.valueOf(TRANSACTION_AMOUNT))
 				.currency(Currency.getInstance("GBP"))
-				.accountId(111L)
+				.accountNum(ACCOUNT_ID_1)
 				.build()
 		);
 
@@ -59,15 +61,18 @@ public class JpaRunner extends BaseComponent implements CommandLineRunner {
 		Statement statement_2 = Statement.builder().transactions(transactionList_2).build();
 
 		Balance balance_1 =
-				Balance.builder().currency(Currency.getInstance("GBP")).amount(BigDecimal.valueOf(1000)).build();
+				Balance.builder().currency(Currency.getInstance("GBP")).amount(BigDecimal.valueOf(BALANCE_AMOUNT)).build();
 		Balance balance_2 =
-				Balance.builder().currency(Currency.getInstance("GBP")).amount(BigDecimal.valueOf(1000)).build();
+				Balance.builder().currency(Currency.getInstance("GBP")).amount(BigDecimal.valueOf(BALANCE_AMOUNT)).build();
 
-		Account account_1 = Account.builder().accountId(111L).statement(statement_1).balance(balance_1).build();
-		Account account_2 = Account.builder().accountId(111L).statement(statement_2).balance(balance_2).build();
+		Account account_1 = Account.builder().accountNum(ACCOUNT_ID_1).statement(statement_1).balance(balance_1).build();
+		Account account_2 = Account.builder().accountNum(ACCOUNT_ID_1).statement(statement_2).balance(balance_2).build();
 
 		balance_1.setAccount(account_1);
 		balance_2.setAccount(account_2);
+
+		balance_1.setAccountNum(ACCOUNT_ID_1);
+		balance_2.setAccountNum(ACCOUNT_ID_2);
 
 		statement_1.setAccount(account_1);
 		statement_2.setAccount(account_2);
